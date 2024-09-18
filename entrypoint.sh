a3a0f029d6a805a3e610864172f25f5734b9187c
Yash Gurav <crce.9374.aids@gmail.com>
2024-09-18 18:26:37 +0000

    new

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
 
-# Exit the script if any command fails
-set -e
-
 echo "=========================="
 
-# Configure Git with the provided user details
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
-
-# Add the GitHub workspace as a safe directory
 git config --global --add safe.directory /github/workspace
 
-# Execute the Python script
 python3 /usr/bin/feed.py
 
-# Stage all changes, commit, and push
-git add -A 
-git commit -m "Update Feed"
+git add -A && git  commit -m "Update Feed"
 
-# Push to the correct branch, defaulting to 'main'
 git push --set-upstream origin main
+
 
 echo "=========================="
